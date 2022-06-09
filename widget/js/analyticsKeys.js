19bc4ba361628ea4d0f1256a55b6989b1d6fae9a
AlaaN-Smadi <alaasmadi1010@gmail.com>
2022-06-09 21:33:02 +0300

    add analytics to handle the number of suggestions and votes

---
+++ widget/js/analyticsKeys.js
@@ -0,0 +1,14 @@
+
+const analyticKeys = Object.freeze({
+    VOTE_NUMBER: {
+        title: "total number of votes",
+        key: "Vote_Number",
+        description: "Number of total votes for all suggestions ",
+    },
+    SUGGESTIONS_NUMBER: {
+        title: "number of suggestions",
+        key: "Suggestions_Number",
+        description: "Number of suggestions for all useres",
+    },
+
+});
